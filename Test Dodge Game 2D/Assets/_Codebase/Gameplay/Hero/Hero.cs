9bc4da673f21755307a8e265b742f9323d718a13
Michael Dvin <idewishortcut@gmail.com>
2024-08-28 22:27:41 +0500

    Add Mover

---
+++ Test Dodge Game 2D/Assets/_Codebase/Gameplay/Hero/Hero.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace _Codebase.Gameplay.Hero
+{
+    public class Hero : MonoBehaviour
+    {
+        [SerializeField] private Mover _mover;
+    }
+}
---
+++ Test Dodge Game 2D/Assets/_Codebase/Gameplay/Hero/Mover.cs
@@ -0,0 +1,67 @@
+using System;
+using _Codebase.Common.ObservableProperty;
+using UnityEngine;
+
+namespace _Codebase.Gameplay.Hero
+{
+    public class Mover : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D _rigidbody;
+        private Transform _transform;
+        
+        private float _moveSpeed;
+        private ObservableProperty<bool> _isMoving = new();
+
+        public void Construct(float moveSpeed)
+        {
+            _moveSpeed = moveSpeed;
+            _transform = transform;
+        }
+
+        public bool Enabled { get; set; } = true;
+
+        private bool IsFacingRight => 
+            _transform.localScale.x > 0;
+
+        public IReadOnlyObservableProperty<bool> IsMoving => _isMoving;
+
+        private void Update()
+        {
+            Move(Vector2.right);
+        }
+
+        private void Move(Vector2 direction)
+        {
+            if (Enabled == false || direction == Vector2.zero)
+            {
+                _isMoving.Value = false;
+                return;
+            }
+            
+            _isMoving.Value = true;
+
+            
+            Vector2 characterPosition = _rigidbody.position;
+            Vector2 newPosition = characterPosition + direction * (_moveSpeed * Time.deltaTime);
+            
+            FlipTowardMoveDirection(characterPosition, newPosition);
+            
+            _rigidbody.MovePosition(newPosition);
+        }
+
+        private void FlipTowardMoveDirection(Vector2 characterPosition, Vector2 newPosition)
+        {
+            if (characterPosition.x < newPosition.x && IsFacingRight == true)
+                Flip();
+            else if (characterPosition.x > newPosition.x && IsFacingRight == false)
+                Flip();
+            
+            void Flip()
+            {
+                Vector3 scale = _transform.localScale;
+                scale.x *= -1;  
+                _transform.localScale = scale;
+            }
+        }
+    }
+}
